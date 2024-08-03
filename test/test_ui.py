3c3f94856f0fba209da3e87438073cbe729450e1
tremarian <p.shaimova@aspro.ru>
2024-08-04 00:19:45 +0500

    new file:   allure-result/0793e514-7878-4fe5-bd8a-64fed9b2c9d8-result.json 	deleted:    allure-result/0ac2c3f2-9d34-44b8-9057-8e8d32a59ed4-container.json 	new file:   allure-result/17218f00-cab5-4bb6-a7f5-bc68538ac192-container.json 	new file:   allure-result/228cad09-e545-4163-b148-5c7894d197e8-container.json 	new file:   allure-result/294259d9-6aed-4d9b-9ed3-234b8f3a89ac-container.json 	deleted:    allure-result/2a822131-dcaf-4b8d-8d73-149dc46833cb-container.json 	deleted:    allure-result/2c1100e3-44c5-4342-9764-7f4dbea03934-container.json 	deleted:    allure-result/3641a6a7-2b5e-4edb-b133-1b67f5b7a74a-result.json 	deleted:    allure-result/422cada3-52b1-4344-87f5-58e3e4dee832-result.json 	new file:   allure-result/4a09967b-e25a-4bcb-aeb0-c1c569d160a5-result.json 	new file:   allure-result/61eb00d3-8fb9-4817-9976-1583e101b81b-result.json 	deleted:    allure-result/6ba231cf-fcb6-4d20-ad35-d2dbe9764e1c-result.json 	new file:   allure-result/6eb16f60-ed0a-4c33-bfdb-52455850a84d-result.json 	deleted:    allure-result/76a9c8af-62a6-40a5-af8f-a4d544f4f22a-container.json 	deleted:    allure-result/8aab86d5-b1cc-414a-b329-b9f8aaca661e-result.json 	deleted:    allure-result/9622e97b-eabd-48e0-a589-ad477682ec60-container.json 	deleted:    allure-result/a6e94e21-841d-4cf4-acad-8538a0a150f1-container.json 	new file:   allure-result/a7d1f2b0-7049-45ab-88e2-5adbe93d34b5-container.json 	deleted:    allure-result/b19975da-0dd9-41d4-bcb8-15d33b3e1e64-result.json 	deleted:    allure-result/c2b7462b-fb95-473c-9fd2-7bba5f050270-result.json 	new file:   allure-result/c802a68d-1e96-4c35-be5d-2d7c45bdd097-container.json 	deleted:    allure-result/cda9ceb3-0640-4481-b8f9-e0940846a92c-container.json 	new file:   allure-result/d0f91c20-40a3-4831-a58f-053242c16ff3-container.json 	new file:   allure-result/d62043fb-cd29-4d76-869f-a7624fb37194-result.json 	new file:   allure-result/e27a5c95-00d1-4aa6-9c96-c3c31e282d65-container.json 	new file:   allure-result/e296bb55-ad46-4106-bf7f-f0c8ed354978-result.json 	deleted:    allure-result/f4346d70-4371-4b72-b4f9-0218b8723e20-container.json 	new file:   allure-result/fe38b19c-5209-42a2-ae64-1d965bef8dfe-container.json 	new file:   test/test_api.py 	modified:   test/test_ui.py

--- test/test_ui.py
+++ test/test_ui.py
@@ -6,18 +6,30 @@ from pages import config
 import allure
 
 
+@allure.id("AGILE-1")
+@allure.feature("Авторизация")
+@allure.severity("blocker")
+@allure.title("Авторизация с валидными данными")
 @pytest.mark.ui_test
 def test_positive_auth():
     """
         Функция для проверки авторизации на протале с валидными данными.
     """
-    browser = webdriver.Chrome()
-    auth = Auth(browser)
-    auth.authorization()
-    config.auth_token = auth.get_auth_cookie('s1')
-    assert auth.get_current_url() == config.portal
+    with allure.step("Открыть портал"):
+        browser = webdriver.Chrome()
+        auth = Auth(browser)
+    with allure.step("Ввести авторизационные данные, нажать кнопку «Войти»"):
+        auth.authorization()
+    with allure.step("Получить токен авторизации"):
+        config.auth_token = auth.get_auth_cookie('s1')
+    with allure.step("Сравнить текущий url с заданным порталом"):
+        assert auth.get_current_url() == config.portal
 
-
+@allure.id("AGILE-2")
+@allure.feature("Создание задачи")
+@allure.story("С указанием валидного названия")
+@allure.severity("critical")
+@allure.title("С указанием названия «{title}»")
 @pytest.mark.ui_test
 @pytest.mark.parametrize('title', [
     ('Название на кириллице'),
@@ -27,21 +39,33 @@ def test_task_create(title):
     """
         Функция для проверки создания задачи с валидными заголовком.
     """
-    browser = webdriver.Chrome()
-    auth = Auth(browser)
-    auth.delete_cookie()
-    auth.add_cookie(config.auth_token)
+    with allure.step("Открыть портал"):
+        browser = webdriver.Chrome()
+        auth = Auth(browser)
+    with allure.step("Авторизоваться"):
+        with allure.step("Очистить куки"):
+            auth.delete_cookie()
+        with allure.step("Добавить токен авторизации в куки"):
+            auth.add_cookie(config.auth_token)
+    with allure.step("Создать задачу"):
+        with allure.step("Открыть бэклог"):
+            task = Task(browser)
+        with allure.step("Нажать кнопку создания задачи"):
+            task.click_create_button()
+        with allure.step("Заполнить заголовок задачи"):
+            task.fill_title(title)
+        with allure.step("Нажать кнопку сохранения"):
+            task.click_submit()
+    with allure.step("Открыть созданную задачу по клику на увекдомление"):
+        task.click_toast()
+    with allure.step("Сравнить заголовок задачи с заданным"):
+        assert task.get_task_title() == title
 
-    task = Task(browser)
-    task.click_create_button()
-
-    task.fill_title(title)
-    task.click_submit()
-    task.click_toast()
-
-    assert task.get_task_title() == title
-
-
+@allure.id("AGILE-2")
+@allure.feature("Создание задачи")
+@allure.story("С указанием трудозатрат")
+@allure.severity("critical")
+@allure.title("С указанием трудозатрат «{estimate}»")
 @pytest.mark.ui_test
 @pytest.mark.parametrize('estimate, result', [
     ('1.5', '1.5'),
@@ -52,17 +76,27 @@ def test_fill_estimate(estimate, result):
     """
         Функция для проверки заполнения трудозатрат при создании задачи.
     """
-    browser = webdriver.Chrome()
-    auth = Auth(browser)
-    auth.delete_cookie()
-    auth.add_cookie(config.auth_token)
-
-    task = Task(browser)
-    task.click_create_button()
-    title = 'Заголовок задачи'
-    task.fill_title(title)
-    task.fill_estimate(estimate)
-    task.click_submit()
-    task.click_toast()
-
-    assert task.get_task_estimate() == result
+    with allure.step("Открыть портал"):
+        browser = webdriver.Chrome()
+        auth = Auth(browser)
+    with allure.step("Авторизоваться"):
+        with allure.step("Очистить куки"):
+            auth.delete_cookie()
+        with allure.step("Добавить токен авторизации в куки"):
+            auth.add_cookie(config.auth_token)
+    with allure.step("Создать задачу"):
+        with allure.step("Открыть бэклог"):
+            task = Task(browser)
+        with allure.step("Нажать кнопку создания задачи"):
+            task.click_create_button()
+        with allure.step("Заполнить заголовок задачи"):
+            title = 'Заголовок задачи'
+            task.fill_title(title)
+        with allure.step("Заполнить трудозатраты задачи"):
+            task.fill_estimate(estimate)
+        with allure.step("Нажать кнопку сохранения"):
+            task.click_submit()
+    with allure.step("Открыть созданную задачу по клику на увекдомление"):
+        task.click_toast()
+    with allure.step("Сравнить трудозатраты задачи с заданными"):
+        assert task.get_task_estimate() == result
